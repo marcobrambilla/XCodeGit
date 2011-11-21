ce34f38d2d59f85c3bfc48dc64631d607c874b67
Marco Brambilla <marco.brambilla@gmail.com>
2011-11-21 18:16:00 +0100

    pippo

---
+++ XCodeGit/Pippo.h
@@ -0,0 +1,13 @@
+//
+//  Pippo.h
+//  XCodeGit
+//
+//  Created by Marco Brambilla on 21/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pippo : NSObject
+
+@end
