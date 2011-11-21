37cadaa57a6579452fb333a850b1ecdf95dcd9ca
Marco Brambilla <marco.brambilla@gmail.com>
2011-11-21 18:07:04 +0100

    Initial Commit

---
+++ XCodeGit/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XCodeGit
+//
+//  Created by Marco Brambilla on 21/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ XCodeGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XCodeGit
+//
+//  Created by Marco Brambilla on 21/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
